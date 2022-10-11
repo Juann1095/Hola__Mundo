ef7299c986eb404035a0998cdf41fc5a725eb991
Juan Romero <slayerforce880@gmail.com>
2022-10-11 18:36:18 -0500

    Initial Commit

---
+++ Hola_Mundo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hola_Mundo
+//
+//  Created by Juan Romero on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hola_Mundo/Hola_MundoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hola_MundoApp.swift
+//  Hola_Mundo
+//
+//  Created by Juan Romero on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Hola_MundoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
